809f2c5a5596d47feffd8ee49a7a91961bcc7788
greciagarciav <garciav.grecia@gmail.com>
2020-05-03 00:40:50 -0500

    feat: ignore coverage directory test: try test without success

--- test/path.spec.js
+++ test/path.spec.js
@@ -2,11 +2,7 @@
 const pathFunctions = require('../lib/path');
 
 describe('test to functions with path module', () => {
-  it('should return true if path is absolute', () => {
-    expect(pathFunctions.IsPathAbsolute()).toBe(true);
-  });
-  it('esta es una prueba de una promesa', (done) => {
-    const promesa = new Promise();
-    promesa.then(done);
+  it('should return false if path is not absolute', () => {
+    expect(pathFunctions.IsPathAbsolute('./lib/options.js')).toBe(false);
   });
 });
