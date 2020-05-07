5af930c93e746b87a7694177aebdc40e0d2480d3
greciagarciav <garciav.grecia@gmail.com>
2020-05-07 08:26:12 -0500

    feat: add function that validate links with fecth

--- api/options.js
+++ api/options.js
@@ -1,2 +1,29 @@
 const fetch = require('node-fetch');
 
+const ValidateLinks = (arrObjects) => {
+  const validatedLinks = arrObjects.map((object) => new Promise((resolve) => {
+    const objValidated = { ...object };
+    fetch(object.href)
+      .then((res) => {
+        if (res.ok) {
+          objValidated.status = res.status;
+          objValidated.ok = res.statusText;
+          resolve(objValidated);
+        } else {
+          objValidated.status = res.status;
+          objValidated.ok = res.statusText;
+          resolve(objValidated);
+        }
+      })
+      .catch(() => {
+        objValidated.status = '';
+        objValidated.ok = 'link does not exist';
+        resolve(objValidated);
+      });
+  }));
+  return Promise.all(validatedLinks);
+};
+
+module.exports = {
+  ValidateLinks,
+};
